602b22ea1c00589fd4c270480a44ed38fb16385c
alex.pinaida <alex.pinaida@rappi.com>
2023-02-27 20:09:28 -0500

    clase  11 y clase 13

---
+++ clase11/ejercicio1/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+import { Server } from 'socket.io';
+import viewsRouter from './routes/views.router.js';
+
+const app = express();
+app.use(express.static(`${__dirname}/public`));
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'handlebars');
+
+app.use('/', viewsRouter);
+
+const server = app.listen(8080, () => console.log('Listening'));
+const io = new Server(server);
+
+const messages = [];
+
+io.on('connection', socket => {
+    socket.on('message', data => {
+        messages.push(data);
+        io.emit('messageLogs', messages);
+    });
+
+    socket.on('authenticated', data => {
+        socket.emit('messageLogs', messages);
+        socket.broadcast.emit('newUserConnected', data);
+    });
+})
