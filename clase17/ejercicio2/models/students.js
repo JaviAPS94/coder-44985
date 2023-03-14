b98a69af8fb1beb05176d98c23fc787d66480d3e
alex.pinaida <alex.pinaida@rappi.com>
2023-03-13 20:23:32 -0500

    clase 17

---
+++ clase17/ejercicio2/models/students.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const studentsCollection = 'students';
+
+
+const studentSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: String,
+    gender: String,
+    grade: Number,
+    group: String
+});
+
+studentSchema.plugin(mongoosePaginate);
+
+const studentModel = mongoose.model(studentsCollection, studentSchema);
+
+export default studentModel;
