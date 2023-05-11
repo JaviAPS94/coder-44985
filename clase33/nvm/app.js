709a1c45650a24b784c0a3502d92f8b397955882
alex.pinaida <alex.pinaida@rappi.com>
2023-05-11 15:06:08 -0500

    clase 33

---
+++ clase33/nvm/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import * as MATH from "44985ap";
+
+const app = express();
+
+//suma
+app.get('/:action', (req, res) => {
+    const operation = req.params.action
+    const { a, b } = req.query;
+    let result;
+    switch (operation) {
+        case 'suma':
+            result = MATH.suma(Number(a), Number(b));
+            break;
+        case 'resta':
+            result = MATH.resta(Number(a), Number(b));
+            break;
+        case 'multiplicacion':
+            result = MATH.multiplicacion(Number(a), Number(b));
+            break;
+        case 'division':
+            result = MATH.division(Number(a), Number(b));
+            break;
+    }
+    res.send({result});
+});
+
+app.listen(8080);
